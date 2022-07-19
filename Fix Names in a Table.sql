ec6ebbfb1fcce52789e078157738c1185f763483
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-07-18 21:05:28 -0700

    sql practices

---
+++ Fix Names in a Table.sql
@@ -0,0 +1,36 @@
+-- Table: Users
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | user_id        | int     |
+-- | name           | varchar |
+-- +----------------+---------+
+-- user_id is the primary key for this table.
+-- This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+-- Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+-- Return the result table ordered by user_id.
+
+-- The query result format is in the following example.
+
+--  Input: 
+-- Users table:
+-- +---------+-------+
+-- | user_id | name  |
+-- +---------+-------+
+-- | 1       | aLice |
+-- | 2       | bOB   |
+-- +---------+-------+
+-- Output: 
+-- +---------+-------+
+-- | user_id | name  |
+-- +---------+-------+
+-- | 1       | Alice |
+-- | 2       | Bob   |
+-- +---------+-------+
+
+SELECT Users.user_id, CONCAT(UPPER(SUBSTR(Users.name, 1,1)), LOWER(SUBSTR(Users.name,2))) AS name
+FROM Users ORDER BY Users.user_id ASC 
