8ba54201ef32e8d9125e92c028acffd962b785ce
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2022-08-12 00:00:21 -0700

    8-11-2022

---
+++ Duplicate Emails.sql
@@ -0,0 +1,49 @@
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write an SQL query to report all the duplicate emails.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
+
+SELECT Email FROM (
+  SELECT Email, Count(Email) as num
+  FROM Person
+  GROUP BY Email
+) as stat
+WHERE num > 1;
+
+SELECT Email FROM Person Group By Email HAVING COUNT(Email) > 1
+
+
